41c7b5e98ce66ce6f689b9caf8211fa608c3d109
Michael Zimmermann <michael.zimmermann@grandcentrix.net>
2023-05-14 07:57:55 +0200

    soc: Add initial SiM3U1xx support
    Includes the SoC, pinctrl, flash and devicetree.
    
    I had to include the flash driver that early because I couldn't make
    Zephyr compile without flash driver nodes in the device tree.
    
    Signed-off-by: Michael Zimmermann <michael.zimmermann@grandcentrix.net>

---
+++ soc/silabs/silabs_sim3/sim3u/soc.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2024 GARDENA GmbH
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef SOC_SILABS_SIM3U_H_
+#define SOC_SILABS_SIM3U_H_
+
+#include <cmsis_core_m_defaults.h>
+
+#endif /* SOC_SILABS_SIM3U_H_ */
