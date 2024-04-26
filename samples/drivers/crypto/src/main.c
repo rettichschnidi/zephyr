fa41afe14e81c2e304476cf7d49bd1450a24ceac
Reto Schneider <reto.schneider@husqvarnagroup.com>
2024-04-26 03:37:17 +0200

    samples: drivers: crypto: Support SiM3U1xx

--- samples/drivers/crypto/src/main.c
+++ samples/drivers/crypto/src/main.c
@@ -30,6 +30,8 @@ LOG_MODULE_REGISTER(main);
 #define CRYPTO_DEV_COMPAT nxp_mcux_dcp
 #elif CONFIG_CRYPTO_NRF_ECB
 #define CRYPTO_DEV_COMPAT nordic_nrf_ecb
+#elif CONFIG_CRYPTO_SI32
+#define CRYPTO_DEV_COMPAT silabs_si32_aes
 #else
 #error "You need to enable one crypto device"
 #endif
