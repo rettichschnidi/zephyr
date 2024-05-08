7f6b082ce302be4c62899f292f1fde799200ac3a
Reto Schneider <reto.schneider@husqvarnagroup.com>
2024-05-08 03:42:44 +0200

    fixup! drivers: crypto: Add initial SiM3U1xx support

--- drivers/crypto/crypto_si32.c
+++ drivers/crypto/crypto_si32.c
@@ -93,7 +93,8 @@ static int crypto_si32_query_hw_caps(const struct device *dev)
 {
 	ARG_UNUSED(dev);
 
-	return (CAP_RAW_KEY | CAP_SEPARATE_IO_BUFS | CAP_INPLACE_OPS | CAP_SYNC_OPS);
+	return (CAP_RAW_KEY | CAP_SEPARATE_IO_BUFS | CAP_INPLACE_OPS | CAP_SYNC_OPS |
+		CAP_NO_IV_PREFIX);
 }
 
 static void crypto_si32_irq_error_handler(const struct device *dev)
